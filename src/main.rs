8b6bee0cb873a53456aa2c0aa07141e7e953daa2
natty-light <75773163+natty-light@users.noreply.github.com>
2023-02-19 14:25:00 -0500

    Add initial arg handling - Will get caught in loop if nothing is piped in, just like cowsay

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-use std::io::{self, BufRead};
+use std::{io::{self, BufRead}, env};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => snail(),
+        2 => execute_action(&args[1]),
+        _ => help(),
+    }
+}
+
+fn help() {
+    let help_string = "Insert Useful information here";
+    println!("{}", help_string)
+}
+
+fn snail() {
     let stdin = io::stdin();
     let snail = r#"
              \          @             _________
@@ -14,7 +28,19 @@ fn main() {
 
     for line in stdin.lock().lines() {
         println!("{}", line.unwrap())
-    }                          
+    }
 
     println!("{}", snail);
+}
+
+fn trail() {
+    println!("{}", "Implement trail feature")
+}
+
+fn execute_action(arg: &str) {
+    match arg {
+        "help" => help(),
+        "trail" => trail(),
+        &_ => println!("Unknown argument {}", arg)
+    }
 }
